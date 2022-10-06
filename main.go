7a5f8e9e20aaa531c32d34c4c39aa8456459819f
USER <user@USERnoMacBook-Pro.local>
2022-10-07 00:57:03 +0900

    init-parser

--- main.go
+++ main.go
@@ -2,8 +2,38 @@ package main
 
 import (
 	"fmt"
+	"os"
+
+	"github.com/urfave/cli"
+)
+
+var (
+	portgroupCommand = cli.Command{
+		Name:  "portgroup",
+		Usage: "portgroupをhogehogeする",
+		Action: func(c *cli.Context) error {
+			fmt.Println("portgroupをhogehoge")
+			return nil
+		},
+	}
+	permissionCommand = cli.Command{
+		Name:  "permission",
+		Usage: "permissionをhogehogeする",
+		Action: func(c *cli.Context) error {
+			fmt.Println("permissionをhogehoge")
+			return nil
+		},
+	}
 )
 
 func main() {
-	fmt.Println("hello")
+	app := cli.NewApp()
+	app.Name = "govc-tools"
+	app.Usage = "vcenter cli tools"
+	app.Version = "0.0.0"
+	app.Commands = []cli.Command{
+		portgroupCommand,
+		permissionCommand,
+	}
+	app.Run(os.Args)
 }
